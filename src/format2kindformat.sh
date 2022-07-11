e5d03c2f414e1a7c4d92ae9e1c1fd382ed95bd9a
Bucchiman <iwabuchi@fun.bio.keio.ac.jp>
2022-07-11 21:18:38 +0900

    format2kind

--- src/format2kindformat.sh
+++ src/format2kindformat.sh
@@ -2,7 +2,7 @@
 #
 # FileName: 	format2kindformat
 # CreatedDate:  2022-07-11 18:02:50 +0900
-# LastModified: 2022-07-11 18:47:32 +0900
+# LastModified: 2022-07-11 21:18:17 +0900
 #
 
 _usage(){
@@ -23,17 +23,26 @@ csv_name=${format_name}".csv"
 output_csv_name=${format_name}"_"${kind}".csv"
 base_dir="../datas/format/"${format_name}
 
+if [[ -e ${base_dir}/${output_csv_name} ]]
+then
+    rm ${base_dir}/${output_csv_name}
+fi
+
 cat ${base_dir}/${csv_name} | awk -v kind=${kind} '
 BEGIN{
-        
 }
 NR==1{
     print $0;
 }
-$5 ~ /[食]/{
-    print $0;
+{
+    if ($5 ~ /["'${kind}'"]/){
+        print $0;
+    }
 }
-'
+' >> ${base_dir}/${output_csv_name}
+#nkf -s --overwrite $base_format/$format_file
+nkf -w8 --overwrite ${base_dir}/${output_csv_name}
+
 
 
 return
